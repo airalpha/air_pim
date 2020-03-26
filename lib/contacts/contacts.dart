7d449e9c2ee2ae97e60ad9a8ebd26d4e887273ab
AIR ALPHA <allphaair@gmail.com>
2020-03-26 15:42:46 +0100

    End contacts ui

--- lib/contacts/contacts.dart
+++ lib/contacts/contacts.dart
@@ -1,8 +1,23 @@
+import 'package:air_pmi/contacts/contacts_model.dart' show contactsModel, ContactsModel;
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class Contacts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ScopedModel(
+      model: contactsModel,
+      child: ScopedModelDescendant<ContactsModel>(
+        builder: (BuildContext inContext, Widget inChild, ContactsModel inModel) {
+          return IndexedStack(
+            index: inModel.stackIndex,
+            children: <Widget>[
+              ContactsList(),
+              ContactsEntry(),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
--- lib/contacts/contacts_model.dart
+++ lib/contacts/contacts_model.dart
@@ -12,3 +12,5 @@ class ContactsModel extends BaseModel {
     notifyListeners();
   }
 }
+
+ContactsModel contactsModel = ContactsModel();
